e0cbc4b6c1db004a7c6f1a771cb387b41290ccf4
Tatsuya Kawakami <usagon620@gmail.com>
2023-08-01 22:24:15 +0900

    詳細画面を作成しました

--- src/components/PikminDetail.vue
+++ src/components/PikminDetail.vue
@@ -29,11 +29,56 @@ const getOnePikminData = async (editNum: number) => {
 getOnePikminData(id)
 </script>
 <template>
-    {{ getData }}
+    <div class="book-content">
+        <div class="book-lower">
+            <div class="book-header">
+                <div class="book-header-column">
+                    <div class="book-header-left-parts">
+                        <h2>{{ getData?.name }}</h2>
+                        <div>
+                            <p>和名：{{ getData?.jName }}</p>
+                            <p>学名：{{ getData?.sName }}</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="book-body">
+                <img class="img" :src="getData?.img" :alt="getData?.name">
+                <div class="book-header-right-parts">
+                        <p>{{ getData?.detail }}</p>
+                </div>
+            </div>
+
+        </div>
+    </div>
     <RouterLink :to="{name:'Hoge', params:{id:1}}">
         <h1>{{ post }}</h1>
     </RouterLink>
     
 </template>
 <style scoped>
+
+.book-lower {
+    background-color: #fff;
+    margin:  0 auto; 
+    max-width: 1000px;
+    padding: 2rem 3.5rem;
+    align-items: center;
+    border: solid 1px black;
+}
+.book-header-column {
+    display: flex;
+    justify-content: flex-start;
+    align-items: flex-start;
+}
+.book-body {
+    display: flex;
+    justify-content: flex-start;
+    align-items: flex-start;
+}
+.img {
+    width: 400px;
+    height: auto;
+    aspect-ratio: 1/1;
+}
 </style>
